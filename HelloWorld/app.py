3ab6432de082c972687eb87a140102ac105eb381
Mateusz <mateusz.miernik1@gmail.com>
2021-09-09 06:56:56 +0200

    Quick commit

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -3,11 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
-
-
 @app.route('/')
 def index():
     return 'Index Page'
